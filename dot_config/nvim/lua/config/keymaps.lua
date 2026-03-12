f8b26c72416ad80e8bdb6473f960bdfb73aed76c
tresespadas <alvaro8038c@gmail.com>
2026-03-12 19:59:33 +0100

    fix: chezmoi implementation

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Universal escape: 'jk' goes to Normal mode from almost anywhere
+vim.keymap.set({ "i", "v", "s", "x", "c", "o", "t" }, "jk", "<Esc>", { noremap = true, silent = true })
--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -51,6 +51,3 @@ require("lazy").setup({
     },
   },
 })
-
--- Colorscheme de Moonfly
-vim.cmd [[colorscheme moonfly]]
